9b4c06d752df50f8ccd5ae8f25cb446a94eaf153
Rachel Warren <raelwa@gmail.com>
2015-10-03 17:47:30 -0400

    took out experience code from show

--- app/controllers/plugs_controller.rb
+++ app/controllers/plugs_controller.rb
@@ -8,12 +8,7 @@ class PlugsController < ApplicationController
   def show
     @plug = Plug.find(params[:id])
     @tags = @plug.tags
-    @experiences =
 
-    Experience.where(plug_id:@plug.id)
-    tags=[]
-
-    @tags = Tag.where(id:tags)
   end
 
 end
